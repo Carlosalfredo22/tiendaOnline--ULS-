6f97f1f0fdc86b181ac9a797fb2994756383283d
Maricela G. Sánchez <149551151+MaricelaS20@users.noreply.github.com>
2025-03-27 15:35:19 -0600

    Maricela
    cambio en detalle pedido

--- database/seeders/detallespedidoSeeder.php
+++ database/seeders/detallespedidoSeeder.php
@@ -10,8 +10,8 @@ class detallespedidoSeeder extends Seeder
     public function run()
     {
         DB::table('detalles_pedido')->insert([
-            ['pedido_id' => 1, 'producto_id' => 1, 'cantidad' => 1, 'precio_unitario' => 299.99, 'subtotal' => 299.99],
-            ['pedido_id' => 1, 'producto_id' => 2, 'cantidad' => 1, 'precio_unitario' => 19.99, 'subtotal' => 19.99],
+            ['pedido_id' => 3, 'producto_id' => 1, 'cantidad' => 1, 'precio_unitario' => 299.99, 'subtotal' => 299.99],
+            ['pedido_id' => 3, 'producto_id' => 2, 'cantidad' => 1, 'precio_unitario' => 19.99, 'subtotal' => 19.99],
         ]);
     }
 }
