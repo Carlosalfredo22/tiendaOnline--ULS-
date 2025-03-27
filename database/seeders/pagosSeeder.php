0cc1d0b13f691983860ae05a8b6d49b4673b1049
Carlosalfredo22 <carlosalfredoalvarezlopez7001@gmail.com>
2025-03-27 15:36:34 -0600

    Tabla pagos seeders
    Cambios-de datos

--- database/seeders/pagosSeeder.php
+++ database/seeders/pagosSeeder.php
@@ -9,7 +9,7 @@ class pagosSeeder extends Seeder
     public function run()
     {
         DB::table('pagos')->insert([
-            ['pedido_id' => 1, 'metodo_pago_id' => 1, 'monto' => 319.98, 'estado' => 'pagado', 'fecha_pago' => now()],
+            ['pedido_id' => 3, 'metodo_pago_id' => 1, 'monto' => 319.98, 'estado' => 'pagado', 'fecha_pago' => now()],
         ]);
     }
 }
