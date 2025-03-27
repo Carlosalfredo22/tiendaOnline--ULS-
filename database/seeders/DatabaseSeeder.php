f6c98fbe73237e6da0f552db9812991fef5289dd
Carlosalfredo22 <carlosalfredoalvarezlopez7001@gmail.com>
2025-03-26 22:08:37 -0600

    DatabaseSeeders
    Cambio realizado..

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,28 +2,23 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
         $this->call([
-            PermissionSeeder::class,
-            RolSeeder::class,
-            UserSeeder::class,
-         
+            rolesSeeder::class,
+            permisosSeeder::class,
+            rolespermisosSeeder::class,
+            usuariosSeeder::class,
+            categoriasSeeder::class,
+            productosSeeder::class,
+            pedidosSeeder::class,
+            detallespedidoSeeder::class,
+            metodospagoSeeder::class,
+            pagosSeeder::class,
         ]);
     }
 }
